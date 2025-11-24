b9cba403e0b933e92f5d35cfe59ec1530f308cd4
Caradobeco <150153723+Caradobeco@users.noreply.github.com>
2025-11-24 07:58:47 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 // Use "type: commonjs" in package.json to use CommonJS modules
-
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
 
- // Define your routes
+app.set('view engine', 'ejs')
+app.set('views',"./views")
 
-app.get('/', (req, res) => {
-
-  res.json({ message: 'Hello from Express on Vercel!' });
-
-});
+require("../rotas/home")(app);
 
 app.listen(port, () => {
-
   console.log(`Example app listening on port ${port}`);
-
 });
-
